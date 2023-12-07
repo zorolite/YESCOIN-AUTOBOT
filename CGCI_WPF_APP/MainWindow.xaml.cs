c7a36688a4abf2412797998cc460f287d8ccef60
Mourad MOUZAI <mourad.mouzai@gmail.com>
2023-12-07 15:29:03 +0100

    Add project files.

---
+++ CGCI_WPF_APP/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using DevExpress.Xpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CGCI_WPF_APP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ThemedWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mainWindow_Closed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ CGCI_WPF_APP/Windows/Auth.xaml.cs
@@ -0,0 +1,41 @@
+﻿using DevExpress.Xpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace CGCI_WPF_APP.Windows
+{
+    /// <summary>
+    /// Interaction logic for Auth.xaml
+    /// </summary>
+    public partial class Auth : ThemedWindow
+    {
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void cancel_btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Validate_btn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            this.Visibility = Visibility.Collapsed;
+            mainWindow.ShowDialog();
+            //this.Close();
+        }
+    }
+}
